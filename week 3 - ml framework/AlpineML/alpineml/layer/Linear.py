a65f5c8d1b60bf39758017737944bebdabe58ddc
AlexPetrusca <petruscalex@gmail.com>
2025-02-18 09:22:20 -0800

    use pytorch initialization

--- week 3 - ml framework/AlpineML/alpineml/layer/Linear.py
+++ week 3 - ml framework/AlpineML/alpineml/layer/Linear.py
@@ -8,13 +8,13 @@ from alpineml.layer.Layer import Layer
 class Linear(Layer):
     def __init__(self, input_dims: int, output_dims: int):
         super().__init__()
-        # # Same initialization that PyTorch uses
-        # scale = math.sqrt(1.0 / input_dims)
-        # self.W = mx.random.uniform(-scale, scale, (output_dims, input_dims))
-        # self.b = mx.random.uniform(-scale, scale, shape=(output_dims, 1))
-
-        self.W = mx.array(np.random.randn(output_dims, input_dims) * 0.01)
-        self.b = mx.array(np.zeros((output_dims, 1)))
+        # Same initialization that PyTorch uses
+        scale = math.sqrt(1.0 / input_dims)
+        self.W = mx.random.uniform(-scale, scale, (output_dims, input_dims))
+        self.b = mx.random.uniform(-scale, scale, shape=(output_dims, 1))
+        # # Custom initialization
+        # self.W = mx.array(np.random.randn(output_dims, input_dims) * 0.01)
+        # self.b = mx.array(np.zeros((output_dims, 1)))
 
     def _forward(self, x_in: mx.array) -> mx.array:
         return self.W @ x_in + self.b
