693bf31edf45b0a45dbd84fa51a93529792e87da
AlexPetrusca <petruscalex@gmail.com>
2025-02-21 10:14:11 -0800

    reword comment

--- week 3 - ml framework/AlpineML/alpineml/function/loss/BinaryCrossEntropyLoss.py
+++ week 3 - ml framework/AlpineML/alpineml/function/loss/BinaryCrossEntropyLoss.py
@@ -4,9 +4,9 @@ import mlx.core as mx
 # todo: even with epsilon=1e-7, this can output massive losses / gradients
 class BinaryCrossEntropyLoss(Function):
     def apply(self, y_pred, y_true, epsilon=1e-7):
-        y_pred = mx.clip(y_pred, epsilon, 1 - epsilon) # computational stability
+        y_pred = mx.clip(y_pred, epsilon, 1 - epsilon) # numerical stability
         return -(y_true * mx.log(y_pred) + (1 - y_true) * mx.log(1 - y_pred))
 
     def apply_derivative(self, y_pred, y_true, epsilon=1e-7):
-        y_pred = mx.clip(y_pred, epsilon, 1 - epsilon) # computational stability
+        y_pred = mx.clip(y_pred, epsilon, 1 - epsilon) # numerical stability
         return (y_pred - y_true) / (y_pred * (1 - y_pred))
