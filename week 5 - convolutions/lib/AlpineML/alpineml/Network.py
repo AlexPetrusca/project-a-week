0042ca384e59225c4a9f8cb99a2b13ce5dc0fd42
AlexPetrusca <petruscalex@gmail.com>
2025-02-25 16:47:54 -0800

    implement Reshape and Flatten layers

--- week 5 - convolutions/lib/AlpineML/alpineml/Network.py
+++ week 5 - convolutions/lib/AlpineML/alpineml/Network.py
@@ -4,15 +4,22 @@ from alpineml.layer.Layer import Layer
 
 
 class Network(ABC):
-    def __init__(self, input_shape, layers=None):
-        self.input_shape = input_shape
+    def __init__(self, input_shape: tuple | int, layers=None):
+        if isinstance(input_shape, int):
+            self.input_shape = (input_shape,)
+        else:
+            self.input_shape = input_shape
         self.output_shape = input_shape
+
         self.layers: list[Layer] = []
         if layers is not None:
             for layer in layers:
                 self.add_layer(layer)
 
     def forward(self, x: mx.array, save_ctx=False) -> mx.array:
+        if x.shape[1:] != self.input_shape:
+            raise ValueError(f"Input shape {x.shape[1:]} does not match network input shape {self.input_shape}.")
+
         for layer in self.layers:
             x = layer.forward(x, save_ctx=save_ctx)
         return x
---
+++ week 5 - convolutions/lib/AlpineML/alpineml/layer/reshape/__init__.py
@@ -0,0 +1,2 @@
+from alpineml.layer.reshape.Flatten import Flatten
+from alpineml.layer.reshape.Reshape import Reshape
