42cb37371e37bd604951d5a4ffb98c12fbafaeb6
AlexPetrusca <petruscalex@gmail.com>
2025-03-02 12:37:00 -0800

    add support for wrapping parameter-less MLX layers

--- week 5 - convolutions/lib/AlpineML/alpineml/layer/__init__.py
+++ week 5 - convolutions/lib/AlpineML/alpineml/layer/__init__.py
@@ -1,6 +1,6 @@
 from alpineml.layer.Layer import Layer
+from alpineml.layer.MLX import MLX
 from alpineml.layer.MyLinear import MyLinear
 from alpineml.layer.MyConv2d import MyConv2d
-from alpineml.layer.MyMaxPool2d import MyMaxPool2d
 import alpineml.layer.core
 import alpineml.layer.reshape
---
+++ week 5 - convolutions/lib/AlpineML/alpineml/layer/conv/__init__.py
@@ -0,0 +1,2 @@
+from alpineml.layer.conv.Conv2d import Conv2d
+from alpineml.layer.conv.MaxPool2d import MaxPool2d
