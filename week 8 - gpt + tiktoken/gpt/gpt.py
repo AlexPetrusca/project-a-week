d2744ad07aa9920ffb1513ac3d73d19e4dffe1e9
AlexPetrusca <petruscalex@gmail.com>
2025-03-18 16:40:40 -0700

    finish tokenizer

--- week 8 - gpt + tiktoken/gpt/gpt.py
+++ week 8 - gpt + tiktoken/gpt/gpt.py
@@ -46,7 +46,7 @@ dropout = 0.2
 # ------------
 
 # wget https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt
-with open('../res/tinyshakespeare.txt', 'r', encoding='utf-8') as f:
+with open('res/tinyshakespeare.txt', 'r', encoding='utf-8') as f:
     text = f.read()
 
 # here are all the unique characters that occur in this text
