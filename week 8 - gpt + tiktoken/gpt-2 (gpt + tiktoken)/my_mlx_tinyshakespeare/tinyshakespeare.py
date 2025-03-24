dbd74c60dd4cb78eab1f6324de714262391fe83c
AlexPetrusca <petruscalex@gmail.com>
2025-03-24 11:55:54 -0700

    tinyshakespeare gpt

--- week 8 - gpt + tiktoken/gpt-2 (gpt + tiktoken)/my_mlx_tinyshakespeare/tinyshakespeare.py
+++ week 8 - gpt + tiktoken/gpt-2 (gpt + tiktoken)/my_mlx_tinyshakespeare/tinyshakespeare.py
@@ -4,7 +4,9 @@ import math
 import time
 from functools import partial
 
-import datasets
+import tiktoken
+
+import dataset
 import mlx.core as mx
 import mlx.nn as nn
 import mlx.optimizers as optim
@@ -78,11 +80,14 @@ weight_decay = 0.1
 
 # Load vocab and dataset:
 # vocab, train, valid, test = datasets.load_dataset(args.dataset)
+tokenizer = tiktoken.get_encoding('gpt2')
+train, val = dataset.load("../res/tinyshakespeare.txt", tokenizer)
 
 # Initialize model:
 model = TransformerLM(
-    vocab_size=len(vocab), num_layers=12, dims=786, num_heads=12, checkpoint=True
+    vocab_size=tokenizer.n_vocab, num_layers=12, dims=768, num_heads=12, checkpoint=True
 )
+model.set_dtype(mx.bfloat16)
 mx.eval(model.parameters())
 nparams = sum(
     x.size for k, x in tree_flatten(model.parameters()) if "embedding" not in k
@@ -95,7 +100,7 @@ def loss_fn(model, x, y, reduce=True):
     return mx.mean(losses) if reduce else mx.mean(losses, axis=(-1, -2))
 
 optimizer = optim.AdamW(
-    learning_rate=lr_warmup, weight_decay=weight_decay
+    learning_rate=learning_rate, weight_decay=weight_decay
 )
 
 def eval_fn(dataset):
@@ -121,27 +126,65 @@ train_iterator = iterate_batches(batch_size, context_size, train)
 losses = []
 tic = time.perf_counter()
 for it, (inputs, targets) in zip(range(max_steps), train_iterator):
-    inputs, targets = map(mx.array, (inputs, targets))
+    model.train()
     optimizer.learning_rate = min(1, it / lr_warmup) * learning_rate
+    inputs, targets = map(mx.array, (inputs, targets))
     loss = step(inputs, targets)
     mx.eval(state)
     losses.append(loss.item())
+
     if (it + 1) % steps_per_report == 0:
         train_loss = np.mean(losses)
         toc = time.perf_counter()
+        n_tok = steps_per_report * batch_size * context_size
         print(
             f"Iter {it + 1}: Train loss {train_loss:.3f}, "
-            f"It/sec {steps_per_report / (toc - tic):.3f}"
+            f"Tok/sec {n_tok / (toc - tic):.3f}"
         )
         losses = []
         tic = time.perf_counter()
-    if (it + 1) % steps_per_eval == 0:
-        val_loss = eval_fn(valid)
-        toc = time.perf_counter()
-        print(
-            f"Iter {it + 1}: "
-            f"Val loss {val_loss:.3f}, "
-            f"Val ppl {math.exp(val_loss):.3f}, "
-            f"Val took {(toc - tic):.3f}s, "
-        )
-        tic = time.perf_counter()
+
+    # if (it + 1) % steps_per_eval == 0:
+    #     model.eval()
+    #     val_loss = eval_fn(val)
+    #     toc = time.perf_counter()
+    #     print(
+    #         f"Iter {it + 1}: "
+    #         f"Val loss {val_loss:.3f}, "
+    #         f"Val ppl {math.exp(val_loss):.3f}, "
+    #         f"Val took {(toc - tic):.3f}s, "
+    #     )
+    #     tic = time.perf_counter()
+
+    if (it + 1) % steps_per_generate == 0:
+        model.eval()
+        num_return_sequences = 4
+        max_length = 32
+        tokens = tokenizer.encode("KING EDWARD IV:")
+        tokens = mx.array(tokens, dtype=mx.int32)  # (8 tokens,)
+        xgen = mx.repeat(mx.expand_dims(tokens, axis=0), num_return_sequences, axis=0)  # (5 rows, 8 tokens)
+        while xgen.shape[1] < max_length:
+            # forward the model to get the logits
+            logits = model(xgen)  # (B, T, vocab_size)
+            # take the logits at the last position
+            logits = logits[:, -1, :]  # (B, vocab_size)
+            # get the probabilities
+            probs = nn.softmax(logits, axis=-1)
+
+            # get the top k probabilities
+            k = 50
+            topk_indices = mx.argsort(logits, axis=-1)[:, -k:]
+            topk_logits = mx.sort(logits, axis=-1)[:, -k:]
+
+            # select a token from the top probabilities
+            ix = mx.random.categorical(topk_logits, num_samples=1)  # (B, 1)
+            xcol = mx.take_along_axis(topk_indices, indices=ix, axis=-1)
+
+            # advance by one token
+            xgen = mx.concatenate([xgen, xcol], axis=1)
+
+        # print the generated text
+        for i in range(num_return_sequences):
+            tokens = xgen[i, :max_length].tolist()
+            decoded = tokenizer.decode(tokens)
+            print(f"sample {i}: {decoded}")
