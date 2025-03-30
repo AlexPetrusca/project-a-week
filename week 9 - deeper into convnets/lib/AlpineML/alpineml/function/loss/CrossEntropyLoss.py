6a96d679f76da6ae6c020c19ebc20325c1936484
AlexPetrusca <petruscalex@gmail.com>
2025-03-30 13:57:43 -0700

    cross-entropy with temperature

--- week 9 - deeper into convnets/lib/AlpineML/alpineml/function/loss/CrossEntropyLoss.py
+++ week 9 - deeper into convnets/lib/AlpineML/alpineml/function/loss/CrossEntropyLoss.py
@@ -5,14 +5,14 @@ import mlx.core as mx
 
 class CrossEntropyLoss(Function):
     @staticmethod
-    def apply(y_pred, y_true, epsilon=1e-7):
-        s = softmax(y_pred)
+    def apply(y_pred, y_true, epsilon=1e-7, temperature=1.0):
+        s = softmax(y_pred, temperature=temperature)
         s = mx.clip(s, epsilon, 1 - epsilon) # numerical stability
         return -y_true * mx.log(s)
 
     @staticmethod
-    def derivative(y_pred, y_true, epsilon=1e-7):
-        s = softmax(y_pred)
+    def derivative(y_pred, y_true, epsilon=1e-7, temperature=1.0):
+        s = softmax(y_pred, temperature=temperature)
         # s = mx.clip(s, epsilon, 1 - epsilon) # numerical stability
         return s - y_true
 
