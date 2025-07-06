82235db9b8885e73f8861460fdb5fcd785bb9d2b
AlexPetrusca <petruscalex@gmail.com>
2025-07-06 10:21:57 -0700

    add support for pages

--- week 15 - chaos game/sketch.ts
+++ week 15 - chaos game/sketch.ts
@@ -1,37 +1,37 @@
 import p5 from 'p5';
-// @ts-ignore
-import vertexShader from './shader.vert?raw';
-// @ts-ignore
-import fragmentShader from './shader.frag?raw';
 
 new p5((p: p5) => {
-    const WIDTH = 1080;
-    const HEIGHT = 720;
-
-    const NUM_PIVOTS = 5; // number of pivots
+    let NUM_PIVOTS = 5; // number of pivots
     let STEP_RATIO = 0.5; // how far to step towards the pivot
+    const NUM_ITERATIONS = 5000; // number of iterations for the chaos walk
+    const NUM_PAGES = 2; // number of different chaos walk methods
 
     let pivots: p5.Vector[] = [];
     let chaosPoint: p5.Vector;
     let hueOffset = 0;
 
     let accumulate = true; // whether to accumulate points or not
-    let myShader: p5.Shader;
+    let page = 0; // current page for different chaos walk methods
 
     p.setup = () => {
-        p.createCanvas(WIDTH, HEIGHT, p.WEBGL);
+        p.createCanvas(1080, 720, p.WEBGL);
         p.colorMode(p.HSB, 360, 100, 100, 100)
-
-        myShader = p.createShader(vertexShader, fragmentShader);
 
         initScene();
         initUI();
     }
 
     function initUI() {
-        const slider: P5Slider = p.createSlider(0, 2, STEP_RATIO, 0.01) as P5Slider;
-        slider.input(() => {
-            STEP_RATIO = slider.value();
+        const stepRatioSlider: P5Slider = p.createSlider(0, 2, STEP_RATIO, 0.005) as P5Slider;
+        stepRatioSlider.input(() => {
+            STEP_RATIO = stepRatioSlider.value();
+            p.background(0);
+        });
+
+        const numPivotsSlider: P5Slider = p.createSlider(3, 10, NUM_PIVOTS, 1) as P5Slider;
+        numPivotsSlider.input(() => {
+            NUM_PIVOTS = numPivotsSlider.value();
+            initScene();
         });
     }
 
@@ -39,7 +39,7 @@ new p5((p: p5) => {
         pivots = [];
 
         let phase = 2 * Math.PI * Math.random();
-        const radius = (HEIGHT - 100)/2;
+        const radius = (p.height - 100)/2;
         for (let i = 0; i < NUM_PIVOTS; i++) {
             const x = radius * Math.cos(phase);
             const y = radius * Math.sin(phase);
@@ -47,31 +47,26 @@ new p5((p: p5) => {
             phase += 2 * Math.PI / NUM_PIVOTS; // spread pivots evenly around a circle
         }
 
-        chaosPoint = p.createVector(p.random(WIDTH), p.random(HEIGHT));
+        chaosPoint = p.createVector(p.random(p.width), p.random(p.height));
         p.background(0);
     }
 
     p.draw = () => {
-        // // Simulate decay by overlaying a translucent black rectangle
-        // p.noStroke();
-        // p.fill(0, 1); // Higher alpha = faster fading
-        // p.rect(-WIDTH, -HEIGHT, 2 * WIDTH, 2 * HEIGHT);
-        //
-        // drawPivots();
-        //
-        // // p.stroke(hueOffset, 100, 100, 100);
-        // // hueOffset = (hueOffset + 1) % 360;
-        // p.stroke(255);
-        // p.strokeWeight(0.25);
-        // // chaosWalkDefault(5000);
-        // chaosWalkUnique(5000);
+        drawPivots();
 
-        p.shader(myShader);
-        // Update any uniforms if needed. For example, passing the elapsed time:
-        myShader.setUniform("u_time", p.millis() / 1000.0);
-
-        // Draw a rectangle that covers the entire canvas, triggering the shader.
-        p.rect(-WIDTH, -HEIGHT, 2 * WIDTH, 2 * HEIGHT);
+        p.stroke(hueOffset, 100, 100, 100);
+        hueOffset = (hueOffset + 1) % 360;
+        p.strokeWeight(0.25);
+        switch (page) {
+            case 0:
+                chaosWalkDefault(NUM_ITERATIONS);
+                break;
+            case 1:
+                chaosWalkUnique(NUM_ITERATIONS);
+                break;
+            default:
+                console.error("Unknown page");
+        }
     }
 
     function drawPivots() {
@@ -118,10 +113,19 @@ new p5((p: p5) => {
         if (event.code === "Space") {
             initScene(); // reset the scene on space key press
         }
-        if (event.code === "ArrowUp") {
+        if (event.code === "Z") {
             accumulate = !accumulate;
-        } else if (event.code === "ArrowDown") {
-            accumulate = !accumulate;
+        }
+        if (event.code === "ArrowDown") {
+            if (page > 0) {
+                page--;
+                p.background(0);
+            }
+        } else if (event.code === "ArrowUp") {
+            if (page < NUM_PAGES - 1) {
+                page++;
+                p.background(0);
+            }
         }
     }
 
